9954a749398accd587aeacde02c4f6f3f817d9fc
rivasdangel <leonardorivasd@gmail.com>
2018-08-03 15:15:16 -0500

    CapperIn Log

--- mex_cue_pcl_line14_wise.js
+++ mex_cue_pcl_line14_wise.js
@@ -323,7 +323,10 @@ client1.on('connect', function(err) {
         CntInLabeller = joinWord(resp.register[10], resp.register[11]);
         CntOutLabeller = joinWord(resp.register[8], resp.register[9]);
         CntInCapper = joinWord(resp.register[14], resp.register[15]);
+        console.log(joinWord(resp.register[12], resp.register[13]));
+        console.log(joinWord(resp.register[13], resp.register[14]));
         console.log(joinWord(resp.register[14], resp.register[15]));
+        console.log(joinWord(resp.register[16], resp.register[17]));
         //------------------------------------------Labeller----------------------------------------------
         Labellerct = CntOutLabeller // NOTE: igualar al contador de salida
         if (!LabellerONS && Labellerct) {
